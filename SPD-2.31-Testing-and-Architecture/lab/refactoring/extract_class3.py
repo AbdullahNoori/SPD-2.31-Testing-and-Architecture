41b9238d5fc6403f0bcddc29f30d0ac3712bdd7e
A.Noori <nooriabdullah86@gmail.com>
2021-02-25 16:30:11 -0800

    Add files via upload

--- SPD-2.31-Testing-and-Architecture/lab/refactoring/extract_class3.py
+++ SPD-2.31-Testing-and-Architecture/lab/refactoring/extract_class3.py
@@ -4,24 +4,24 @@ WELL_DONE = 3000
 MEDIUM = 2500
 COOKED_CONSTANT = 0.05
 
+cooking_criteria = "time","temp", "pressure", "desired_state"
 
-def is_cookeding_criteria_satisfied(time, temperature, 
-                                    pressure, desired_state):
-    return is_well_done(time, temperature, pressure, desired_state) or \
-           is_medium(time, temperature, pressure, desired_state)
+def is_cookeding_criteria_satisfied(cooking_criteria):
+    return is_well_done(cooking_criteria) or \
+           is_medium(cooking_criteria)
 
 
-def is_well_done(time, temperature, pressure, desired_state):    
+def is_well_done(cooking_criteria):    
     return desired_state == 'well-done' and  \
-           get_cooking_progress(time, temperature, pressure) >= WELL_DONE
+           get_cooking_progress(time, temp, pressure) >= WELL_DONE
 
 
-def is_medium(time, temperature, pressure, desired_state):
+def is_medium(cooking_criteria):
     return desired_state == 'medium' and  \
-           get_cooking_progress(time, temperature, pressure) >= MEDIUM
+           get_cooking_progress(time, temp, pressure) >= MEDIUM
 
-def get_cooking_progress(time, temperature, pressure):
-    return time * temperature * pressure * COOKED_CONSTANT
+def get_cooking_progress(time, temp, pressure):
+    return time * temp * pressure * COOKED_CONSTANT
 
 
 time = 30 # [min]
@@ -29,7 +29,7 @@ temp = 103 # [celcius]
 pressure = 20 # [psi]
 desired_state = 'well-done'
 
-if is_cookeding_criteria_satisfied(time, temp, pressure, desired_state):
+if is_cookeding_criteria_satisfied(cooking_criteria):
     print('cooking is done.')
 else:
     print('ongoing cooking.')
